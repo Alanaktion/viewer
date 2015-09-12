afa3b58a5efda1bc546aa92772631d6d390a7b22
Alan Hardman <alanaktion@gmail.com>
2015-09-11 20:24:35 -0600

    Working image viewer

---
+++ Viewer/frmMain.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Viewer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showInExplorerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.lblLoading = new System.Windows.Forms.ToolStripLabel();
+            this.btnOpen = new System.Windows.Forms.ToolStripButton();
+            this.btnPrev = new System.Windows.Forms.ToolStripButton();
+            this.btnNext = new System.Windows.Forms.ToolStripButton();
+            this.pbox = new System.Windows.Forms.PictureBox();
+            this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showInExplorerToolStripMenuItem,
+            this.propertiesToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(162, 70);
+            // 
+            // showInExplorerToolStripMenuItem
+            // 
+            this.showInExplorerToolStripMenuItem.Name = "showInExplorerToolStripMenuItem";
+            this.showInExplorerToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.showInExplorerToolStripMenuItem.Text = "Show in Explorer";
+            this.showInExplorerToolStripMenuItem.Click += new System.EventHandler(this.showInExplorerToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnOpen,
+            this.toolStripSeparator1,
+            this.btnPrev,
+            this.btnNext,
+            this.toolStripSeparator2,
+            this.lblLoading});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(659, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(59, 22);
+            this.lblLoading.Text = "Loading...";
+            this.lblLoading.Visible = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnOpen.Image = global::Viewer.Properties.Resources.folder_Open_16xMD;
+            this.btnOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(23, 22);
+            this.btnOpen.Text = "Open";
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnPrev.Image = global::Viewer.Properties.Resources.arrow_back_color_16xLG;
+            this.btnPrev.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(23, 22);
+            this.btnPrev.Text = "Previous";
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnNext.Image = global::Viewer.Properties.Resources.arrow_Forward_color_16xLG;
+            this.btnNext.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(23, 22);
+            this.btnNext.Text = "Next";
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // pbox
+            // 
+            this.pbox.ContextMenuStrip = this.contextMenuStrip1;
+            this.pbox.Image = global::Viewer.Properties.Resources.Outlook;
+            this.pbox.InitialImage = global::Viewer.Properties.Resources.Outlook;
+            this.pbox.Location = new System.Drawing.Point(12, 28);
+            this.pbox.Name = "pbox";
+            this.pbox.Size = new System.Drawing.Size(64, 64);
+            this.pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbox.TabIndex = 0;
+            this.pbox.TabStop = false;
+            // 
+            // propertiesToolStripMenuItem
+            // 
+            this.propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
+            this.propertiesToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.propertiesToolStripMenuItem.Text = "Properties";
+            this.propertiesToolStripMenuItem.Click += new System.EventHandler(this.propertiesToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(659, 435);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Viewer";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbox;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnOpen;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showInExplorerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnPrev;
+        private System.Windows.Forms.ToolStripButton btnNext;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel lblLoading;
+        private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem;
+    }
+}
+
---
+++ Viewer/frmMain.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.ComponentModel;
+
+namespace Viewer {
+    public partial class frmMain : Form {
+
+        private string[] files;
+        private int index;
+
+        public frmMain() {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e) {
+            // Get image from cli args, if any
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length >= 1) {
+                if (File.Exists(args[args.Length - 1])) {
+                    string filePath = args[args.Length - 1];
+                    openImage(filePath);
+                }
+            }
+
+            // Initialize hotkeys
+            KeyPreview = true;
+            KeyDown += new KeyEventHandler(frmMain_KeyDown);
+
+            // Handle events needing a resize
+            Resize += new EventHandler(frmMain_Resize);
+            pbox.LoadCompleted += new AsyncCompletedEventHandler(pbox_LoadCompleted);
+        }
+
+        private void frmMain_Resize(object sender, EventArgs e) {
+            resizePBox();
+        }
+
+        private void frmMain_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Control && e.KeyCode == Keys.O) { // Open
+                e.SuppressKeyPress = true;
+                showOpenDialog();
+            }
+            if (e.KeyCode == Keys.Left) {
+                e.SuppressKeyPress = true;
+                switchImage(-1);
+            }
+            if (e.KeyCode == Keys.Right) {
+                e.SuppressKeyPress = true;
+                switchImage(1);
+            }
+            if (e.KeyCode == Keys.D0 || e.KeyCode == Keys.NumPad0) {
+                e.SuppressKeyPress = true;
+                pbox.SizeMode = PictureBoxSizeMode.Zoom;
+                resizePBox();
+            }
+            if (e.KeyCode == Keys.Oemplus || e.KeyCode == Keys.OemPeriod) {
+                e.SuppressKeyPress = true;
+                pbox.SizeMode = PictureBoxSizeMode.AutoSize;
+                resizePBox();
+            }
+            if(e.Alt && e.KeyCode == Keys.Enter) {
+                e.SuppressKeyPress = true;
+                showImageProperties();
+            }
+        }
+
+        public void openImage(string path) {
+            // Get other files in directory
+            string[] files = Directory.GetFiles(Path.GetDirectoryName(path));
+
+            // Filter to only images
+            string[] images = files.Where(s => s.Length > 4 && (
+                    s.Substring(s.Length - 4, 4) == ".jpg" ||
+                    s.Substring(s.Length - 4, 4) == ".png" ||
+                    s.Substring(s.Length - 4, 4) == ".gif" ||
+                    s.Substring(s.Length - 4, 4) == ".bmp")
+                ).ToArray();
+            
+            // Sort resulting array
+            Array.Sort(images, StringComparer.CurrentCultureIgnoreCase);
+            this.files = images;
+
+            // Set this.index to position of selected image in directory
+            index = Array.IndexOf(images, path);
+
+            // Load image into pbox
+            pbox.ImageLocation = path;
+            lblLoading.Visible = true;
+        }
+
+        public void switchImage(int change) {
+            index += change;
+            if (index < 0) {
+                index = files.Length - 1;
+            } else if (index >= files.Length) {
+                index = 0;
+            }
+            pbox.ImageLocation = files[index];
+            lblLoading.Visible = true;
+        }
+
+        public void resizePBox() {
+            if (pbox.SizeMode == PictureBoxSizeMode.AutoSize) {
+                bool needsScroll = false;
+                if (pbox.Width <= ClientSize.Width) {
+                    pbox.Left = (ClientSize.Width - pbox.Width) / 2;
+                } else {
+                    pbox.Left = 0;
+                    needsScroll = true;
+                }
+                if (pbox.Height <= ClientSize.Height - toolStrip1.Height) {
+                    pbox.Top = (ClientSize.Height - pbox.Height) / 2 + toolStrip1.Height;
+                } else {
+                    pbox.Top = toolStrip1.Height;
+                    needsScroll = true;
+                }
+                AutoScroll = needsScroll;
+            } else if(pbox.SizeMode == PictureBoxSizeMode.Zoom) {
+                pbox.Top = toolStrip1.Height;
+                pbox.Left = 0;
+                pbox.Width = ClientSize.Width;
+                pbox.Height = ClientSize.Height - toolStrip1.Height;
+                AutoScroll = false;
+            }
+        }
+
+        public void showOpenDialog() {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Images|*.jpg;*.png;*.gif;*.bmp"; // |All files|*.*
+            dlg.FilterIndex = 1;
+            dlg.RestoreDirectory = true;
+
+            if (dlg.ShowDialog() == DialogResult.OK) {
+                openImage(dlg.FileName);
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e) {
+            showOpenDialog();
+        }
+
+        private void pbox_LoadCompleted(object sender, AsyncCompletedEventArgs e) {
+            resizePBox();
+            if (files.ElementAtOrDefault(index) != null) {
+                Text = Path.GetFileName(files[index]) + " - Viewer";
+            }
+            lblLoading.Visible = false;
+        }
+
+        private void showInExplorerToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (files.ElementAtOrDefault(index) != null) {
+                Process.Start("explorer.exe", string.Format("/select,\"{0}\"", pbox.ImageLocation));
+            }
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e) {
+            switchImage(-1);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e) {
+            switchImage(1);
+        }
+
+        private void propertiesToolStripMenuItem_Click(object sender, EventArgs e) {
+            showImageProperties();
+        }
+
+        public void showImageProperties() {
+            if (files.ElementAtOrDefault(index) != null) {
+                FileInfo info = new FileInfo(pbox.ImageLocation);
+                string text = Path.GetFileName(pbox.ImageLocation);
+                text += "\r\n";
+                text += "\r\nCreated: " + info.CreationTime;
+                text += "\r\nSize: " + (info.Length / 1024) + " KB";
+                text += "\r\nDimensions: " + pbox.Image.Width + "x" + pbox.Image.Height;
+                MessageBox.Show(text, Path.GetFileName(pbox.ImageLocation) + " Properties");
+            }
+        }
+    }
+}
